61fa3fffb1a002d8e9b814bb611e8dca693afbcc
ulrikHesmyr <ulrik.hesmyr@nav.no>
2024-06-19 12:58:19 +0200

    wooohooo

--- main.go
+++ main.go
@@ -1,25 +1,28 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log/slog"
+	"os"
 
+	"github.com/google/uuid"
 	"github.com/navikt/go-leesah"
 )
 
 func main() {
 	os.Setenv("KAFKA_GROUP_ID", uuid.New().String())
 
-	var config leesah.RapidConfig{}
+	var config leesah.RapidConfig
 	flag.StringVar(&config.Brokers, "brokers", os.Getenv("KAFKA_BROKERS"), "Kafka broker")
 	flag.StringVar(&config.Topic, "topic", os.Getenv("KAFKA_TOPIC"), "Kafka topic")
 	flag.StringVar(&config.GroupID, "group-id", os.Getenv("KAFKA_GROUP_ID"), "Kafka group ID")
 	flag.StringVar(&config.KafkaCertPath, "kafka-cert-path", os.Getenv("KAFKA_CERTIFICATE_PATH"), "Path to Kafka certificate")
 	flag.StringVar(&config.KafkaPrivateKeyPath, "kafka-private-key-path", os.Getenv("KAFKA_PRIVATE_KEY_PATH"), "Path to Kafka private key")
 	flag.StringVar(&config.KafkaCAPath, "kafka-ca-path", os.Getenv("KAFKA_CA_PATH"), "Path to Kafka CA certificate")
-	config.Log = slog.New()
+	config.Log = slog.New(slog.NewJSONHandler(os.Stdout, nil))
 
-	rapid, err := leesah.NewRapid("my-go-team", config)
+	rapid, err := leesah.NewRapid("team-teamwork", config)
 	if err != nil {
 		slog.Error(fmt.Sprintf("failed to create rapid: %s", err))
 		return
@@ -37,7 +40,7 @@ func Answer(question leesah.Question, log *slog.Logger) (string, bool) {
 
 	switch question.Category {
 	case "team-registration":
-		return "insert-hex-code-here", true
+		return "#371F76", true
 	}
 
 	return "", false
